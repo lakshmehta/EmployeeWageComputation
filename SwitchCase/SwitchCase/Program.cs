0b561fa3fdd884e83d1b54b33f7079aa2d33bc3c
unknown <lakshmehta06@gmail.com>
2021-03-07 17:25:48 +0530

    [Laksh mehta added Fourth]

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+           switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0 ;
+                    break;  
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp Wage: " + empWage);
+            Console.ReadKey();
+        }
+    }
+}
